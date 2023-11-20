3f450f94d096600d3084e7040ab3a46c61e71b2f
Trương Thanh Tùng <ttungbmt@gmail.com>
2023-11-20 18:29:35 +0700

    Update changes

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'tsup'
 import { legacyConfig, modernConfig } from '@laragis/tsup-config'
 
 export default defineConfig([
-  modernConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
-  legacyConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
+  modernConfig({ entry: ['src/**/*.{ts,tsx}'] }),
+  legacyConfig({ entry: ['src/**/*.{ts,tsx}'] }),
 ])
